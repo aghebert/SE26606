1bc6fb22b51bc41312bed195f830b9da3ce4b475
Hebert <aghebert@email.neit.edu>
2017-10-02 11:03:38 -0400

    halt syntax, one forloop

--- mult.php
+++ mult.php
@@ -1,10 +1,25 @@
+<?php
+$table = "<table>\n"; //empty table var
+for ($rows =1; $rows <= 5; $rows++){
+    $table .= "\t<tr>";
+    for ($cols=1; $cols<=5; $cols++):
+        $table .= "<td>" . $rows * $cols . "</td>";
+    endfor;
+    $table .= "</tr>\n";
+}
+
+$table .= "</table>";
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <title>Title</title>
+
 </head>
 <body>
-
+<?php echo $table; ?>
 </body>
+
 </html>
