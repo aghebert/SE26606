fcca91b78b6340588099781858355cfb0fc965d0
Aaron <aghebert@email.neit.edu>
2017-10-14 08:46:42 -0400

    Changed Get to Post to see what it was like

--- phpAssignment2.php
+++ phpAssignment2.php
@@ -5,3 +5,51 @@
  * Date: 10/13/17
  * Time: 7:38 PM
  */
+$dsn = "mysql:host=localhost;dbname=PHPClassFall2017";
+$username = "root";
+
+try{
+    $db = new PDO($dsn, $username);
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $submit = $_POST['submit'] ?? "";
+    if ($submit == "Do it"){
+        $id = $_POST['id'] ?? "";
+        $firstname = $_POST['firstname'] ?? "";
+        $lastname = $_POST['lastname'] ?? "";
+        $dob = $_POST['dob'] ?? "";
+        $height = $_POST['height'] ?? "";
+
+        $sql = $db->prepare("INSERT INTO actors VALUES (null, :firstname, :lastname, :dob, :height)");
+        $sql->bindParam(':firstname', $firstname);
+        $sql->bindParam(':lastname', $lastname);
+        $sql->bindParam(':dob', $dob);
+        $sql->bindParam(':height', $height);
+
+        $sql->execute();
+
+        echo $sql->rowCount() . " rows inserted";
+
+    }
+} catch (PDOException $e) {
+    die("There was a problem connecting to the DB.");
+}
+
+?>
+
+<form method="POST" action="#">
+    <input type="text" name="firstname" value="" /><br />
+    <input type="text" name="lastname" value="" /><br />
+    <input type="text" name="dob" value="" /><br />
+    <input type="text" name="height" value="" /><br />
+    <input type="submit" id="submitactors" name="submit" value="Do it" />
+</form>
+
+<?php
+$sql = $db->prepare("SELECT * FROM actors");
+$sql->execute();
+$results = $sql->fetchAll();
+if (count($results)){
+    foreach ($results as $actor){
+        print_r($actor);
+    }
+}
