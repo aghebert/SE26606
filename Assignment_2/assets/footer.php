ba8a4771eef42930f2687e2c8d28770dacd8dd87
Aaron <aghebert@email.neit.edu>
2017-10-16 08:20:55 -0400

    Basic Template

---
+++ Assignment_2/assets/footer.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: aaronhebert
+ * Date: 10/16/17
+ * Time: 8:14 AM
+ */
+
+?>
+
+</section>
+<footer>
+    Copyright &copy; <?php echo date('Y'); ?> Aaron Hebert. All rights reserved
+</footer>
+</body>
+</html>
---
+++ Assignment_2/assets/header.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: aaronhebert
+ * Date: 10/16/17
+ * Time: 8:12 AM
+ */
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+    <!-- css links go here-->
+    <!-- javascript links go here -->
+    <nav></nav>
+</head>
+<body>
+<section>
